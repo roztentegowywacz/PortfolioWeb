34e301d47b7ec2ffa8f63355e803cfd3146de42e
Tomasz Klag <tomek.klag@gmail.com>
2018-10-05 14:11:15 +0200

    Add authentication controller with Login and Logout methods

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,38 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using PortfolioWeb.ViewModels;
+
+namespace PortfolioWeb.Controllers
+{
+    public class AuthController : Controller
+    {
+        private SignInManager<IdentityUser> _signInManager;
+
+        public AuthController(SignInManager<IdentityUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+        
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View(new LoginViewModel());
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel vm)
+        {
+            var result =  await _signInManager.PasswordSignInAsync(vm.UserName, vm.Password, false, false);
+            
+            return RedirectToAction("Index", "Home");
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
