4c758796f6e9c2e5ca06245e771738c250a2c802
Tomasz Klag <tomek.klag@gmail.com>
2018-10-15 10:13:15 +0200

    Clean up the Layout and repair Trumbowyg bug

--- wwwroot/js/app.js
+++ wwwroot/js/app.js
@@ -4,13 +4,3 @@ document.querySelector('#toggle').onclick = function() {
     document.querySelector('#overlay').classList.toggle('open');
   };
   // Navigation script - END
-
-// Initialize Mixitup (gallery and filter)
-var containerEl = document.querySelector('[data-ref="mixitup-container"]');
-
-var mixer = mixitup(containerEl, {
-    selectors: {
-        target: '[data-ref="mixitup-target"]'
-    }
-});
-// Initialize Mixitup - END
